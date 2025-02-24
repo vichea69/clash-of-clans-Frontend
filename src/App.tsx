fc439783df9006ea1860dd973dfbf6f98531ee24
Vichea69 <smart.vichea@gmail.com>
2025-02-24 17:10:30 +0700

    Update project dependencies and configuration for Tailwind CSS and shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-center">Hello World</h1>
+      <Button variant="default">Click me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button variant="link">Click me</Button>
     </div>
   );
 }
