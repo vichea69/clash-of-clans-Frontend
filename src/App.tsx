17884a84139b72875ae85a58afcd57aae6b01e73
Vichea69 <smart.vichea@gmail.com>
2025-02-25 19:33:05 +0700

    Refactor App routing and theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "./components/theme-provider";
 import AppRouter from "./routes/AppRouter";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <ThemeProvider defaultTheme="system" storageKey="app-theme">
-        <Routes>
-          <Route path="/*" element={<AppRouter />} />
-        </Routes>
-      </ThemeProvider>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
